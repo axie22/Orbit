3052d0032daacffea8fc3d0712717990f9b518b0
axie22 <alexxie9667@gmail.com>
2025-11-25 12:28:11 -0500

    Created custom navbar with heroui

---
+++ interv-ai/app/components/CustomNavbar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import {  Navbar,   
+    NavbarBrand,   
+    NavbarContent,   
+    NavbarItem,
+    Button} from "@heroui/react";
+
+import Link from 'next/link'
+
+export default function CustomNavbar() {
+    return (
+        <Navbar position="static">
+            <NavbarBrand>
+                <h1 className="font-bold">IntervAI</h1>
+            </NavbarBrand>
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                    Features
+                    </Link>
+                </NavbarItem>
+                <NavbarItem isActive>
+                    <Link aria-current="page" href="#">
+                    Customers
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                    Integrations
+                    </Link>
+                </NavbarItem>
+                </NavbarContent>
+                <NavbarContent justify="end">
+                <NavbarItem className="hidden lg:flex">
+                    <Link href="#">Login</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Button as={Link} color="primary" href="#" variant="flat">
+                    Sign Up
+                    </Button>
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    );
+}
