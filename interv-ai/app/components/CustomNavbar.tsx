1627169baa55c03d8297ed66dcc0fb243a234acb
axie22 <alexxie9667@gmail.com>
2025-11-25 12:35:35 -0500

    Minimal nav bar

--- interv-ai/app/components/CustomNavbar.tsx
+++ interv-ai/app/components/CustomNavbar.tsx
@@ -17,17 +17,7 @@ export default function CustomNavbar() {
             <NavbarContent className="hidden sm:flex gap-4" justify="center">
                 <NavbarItem>
                     <Link color="foreground" href="#">
-                    Features
-                    </Link>
-                </NavbarItem>
-                <NavbarItem isActive>
-                    <Link aria-current="page" href="#">
-                    Customers
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link color="foreground" href="#">
-                    Integrations
+                        Practice
                     </Link>
                 </NavbarItem>
                 </NavbarContent>
